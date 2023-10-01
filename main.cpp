4d284e8cdc2abc386de783054a7c9b988b222a0a
leo <leopold@lchappuis.fr>
2023-10-01 14:39:58 +0200

    added src files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by leo on 10/1/23.
+//
+
+#include "main.h"
+
+
+int main(){
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by leo on 10/1/23.
+//
+
+#ifndef LO21_SPLENDOR_DUEL_MAIN_H
+#define LO21_SPLENDOR_DUEL_MAIN_H
+
+#endif //LO21_SPLENDOR_DUEL_MAIN_H
