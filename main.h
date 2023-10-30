895033cf6d1935ada950f21f3292de3d4c2fb6aa
AubinVert <barbe.tresorier-0t@icloud.com>
2023-10-30 16:13:20 +0100

    Déclarations de toutes les classes

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef LO21_SPLENDOR_DUEL_MAIN_H
 #define LO21_SPLENDOR_DUEL_MAIN_H
+#include <iostream>
 
+using namespace std;
 // classe par énumération à créer: capacité, bonus_couleur, couleur (pour les jetons)
+
+enum class Capacite {rejouer, voler_pion_adverse, prendre_privilege, prendre_sur_plateau, joker};// joker dans capacité et bonus?
+enum class Couleur {bleu, rouge, vert, blanc, noir};
+enum class Bonus_couleur {bleu, rouge, vert, blanc, noir, joker};
 
 class Carte{
     Capacite capacite;
@@ -72,6 +78,41 @@ class Plateau{
     int nb;
     int max;
     Jeton * jetons[][]; // vérifier
+};
+
+class Joueur{
+    string nom;
+    int nb_points;
+    int nb_courones;
+    int nb_cartes_j;
+    int nb_cartes_r;
+    int nb_privileges;
+
+    int max_nb_jetons;
+    int max_nb_cartes_j;
+
+    Carte_joaillerie** cartes_joaillerie;
+    Carte_royale* cartes_royale[3]; // ok pour agrégation?
+    Jeton ** jetons;
+    Privilege* privileges[3]; // ok pour agrégation?
+
+
+};
+
+class Jeu{
+    bool est_termine;
+    bool contre_IA;
+    Joueur* qui_joue;
+    Joueur* adversaire;
+
+    // tableaux contenants tous les objets à la création du jeu
+    Sac* sac;
+    Plateau* plateau;
+    Carte* cartes[67]; // contient toutes les cartes du jeu
+    Jeton* jetons[15]; // contient tous les jetons
+    Privilege* privileges[3];
+
+
 };
 
 
