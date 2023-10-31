053df734c62b7f1bdd32dcb262f853e31e541984
AubinVert <barbe.tresorier-0t@icloud.com>
2023-10-31 11:46:51 +0100

    Déclarations méthodes

--- joueur.h
+++ joueur.h
@@ -20,6 +20,26 @@ class Joueur{
     Jeton ** jetons;
     Privilege* privileges[3]; // ok pour agrégation?
 
+public:
+    const int get_nb_points()const{return nb_points};
+    void set_points(int nb){nb_points = nb;};
+    const int get_nb_couronnes()const{return nb_courones};
+    void set_nb_couronnes(int nb){nb_courones = nb;};
+    const int get_nb_privileges()const{return nb_privileges};
+    void set_nb_privileges(int nb){nb_privileges = nb;};
+
+    // utilisation du design pattern iterator pour get cartes_j
+    // utilisation du design pattern iterator pour get cartes_r
+    // utilisation du design pattern iterator pour get jetons
+    // utilisation du design pattern iterator pour get privilèges
+
+    void acheter_carte();
+    void mettre_jetons_dans_sac();
+    void piocher_jetons();
+    void obtenir_carte_royale();
+
+
+
 
 };
 #endif //LO21_SPLENDOR_DUEL_JOUEUR_H
