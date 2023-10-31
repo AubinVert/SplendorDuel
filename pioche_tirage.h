62a39ed13bfa1277101febaef93a83e9e140a608
AubinVert <barbe.tresorier-0t@icloud.com>
2023-10-31 11:53:56 +0100

    Déclaration méthodes

--- pioche_tirage.h
+++ pioche_tirage.h
@@ -1,7 +1,7 @@
 #ifndef LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
 #define LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
 #include <iostream>
-
+#include "carte.h"
 using namespace std;
 
 class Pioche{
@@ -9,6 +9,8 @@ class Pioche{
     int nb;
     int max;
     Carte_joaillerie ** cartes; // vérifier si on peut pas mettre déjà une taille
+public:
+    bool est_vide(){return nb==0;}
 };
 
 class Tirage{
@@ -18,6 +20,9 @@ class Tirage{
     int max;
     Carte_joaillerie ** cartes;
     Pioche * pioche;
+public:
+    void piocher();
+    // design pattern iterator pour get tirage
 };
 
 #endif //LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
