97f36a5131023069696fd8eb74920a9847ea928e
AubinVert <barbe.tresorier-0t@icloud.com>
2023-11-01 15:34:27 +0100

    Corrections

--- classes/jetons.h
+++ classes/jetons.h
@@ -9,12 +9,11 @@ using namespace std;
 class Jeton{
     // il faut garantir l'unicité de chaque jeton
     int id;
-
 public:
-    Jeton(int id)=default;
+    Jeton(int id):id(id){}
     ~Jeton()=default;
-    operator=(const Jeton& jet){}=delete;
-    Jeton(const Jeton& jet){}=delete;
+    Jeton& operator=(const Jeton& jet)=delete;
+    Jeton(const Jeton& jet)=delete;
 
     const int get_id()const{return id;}
 };
@@ -22,23 +21,20 @@ public:
 class Jeton_perle:public Jeton{
 public:
     Jeton_perle(int id):Jeton(id){
-        if(id<1)||(id>25){
+        if((id<1)||(id>25)){
             throw SplendorException("Id de jeton négatif!");
         }
     }
-    operator=(const Jeton_perle& jet){}=delete;
-    Jeton_perle(const Jeton_perle& jet){}=delete;
+
 };
 
 class Jeton_or:public Jeton{
 public:
     Jeton_or(int id):Jeton(id){
-        if(id<1)||(id>25){
+        if((id<1)||(id>25)){
             throw SplendorException("Id de jeton négatif!");
         }
     };
-    operator=(const Jeton_or& jet){}=delete;
-    Jeton_or(const Jeton_or& jet){}=delete;
 };
 
 class Jeton_gemme:public Jeton{
@@ -46,12 +42,10 @@ class Jeton_gemme:public Jeton{
 
 public:
     Jeton_gemme(int id, Couleur coul):Jeton(id),couleur(coul){
-        if(id<1)||(id>25){
+        if((id<1)||(id>25)){
             throw SplendorException("Id de jeton négatif!");
         }
     }
-    operator=(const Jeton_gemme& jet){}=delete;
-    Jeton_gemme(const Jeton_gemme& jet){}=delete;
 
     const Couleur& get_couleur() const{return couleur;}
     void set_couleur(const Couleur& coul){couleur = coul;}
