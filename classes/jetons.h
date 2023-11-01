3c7e89f7de2a3a8c44c659f140c5988489a7c20d
AubinVert <barbe.tresorier-0t@icloud.com>
2023-11-01 11:59:01 +0100

    Petites corrections

--- classes/jetons.h
+++ classes/jetons.h
@@ -14,16 +14,32 @@ public:
 };
 
 class Jeton_perle:public Jeton{
-
+public:
+    Jeton_perle(int id):id(id){
+        if(id<1)||(id>15){
+            throw "id non-autorisé";
+        }
+    };
 };
 
 class Jeton_or:public Jeton{
-
+public:
+    Jeton_or(int id):id(id){
+        if(id<1)||(id>15){
+            throw "id non-autorisé";
+        }
+    };
 };
 
 class Jeton_gemme:public Jeton{
     Couleur couleur;
+
 public:
+    Jeton_gemme(int id, Couleur coul):id(id),couleur(coul){
+        if(id<1)||(id>15){
+            throw "id non-autorisé";
+        }
+    }
     const Couleur& get_couleur() const{return couleur;}
     void set_couleur(const Couleur& coul){couleur = coul;}
 };
