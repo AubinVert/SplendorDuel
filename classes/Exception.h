d6f7b9e77f982cfc653309f61df29368640050b7
AubinVert <barbe.tresorier-0t@icloud.com>
2023-11-01 14:37:45 +0100

    init

---
+++ classes/Exception.h
@@ -0,0 +1,18 @@
+//
+// Created by leo on 01/11/2023.
+//
+
+#ifndef LO21_SPLENDOR_DUEL_EXCEPTION_H
+#define LO21_SPLENDOR_DUEL_EXCEPTION_H
+#include <iostream>
+
+class SplendorException{
+    std::string info;
+public:
+    SplendorException(const std::string &str) : info(str){}
+    std::string getInfos() const{return info;}
+};
+
+
+
+#endif //LO21_SPLENDOR_DUEL_EXCEPTION_H
