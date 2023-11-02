018f6712512dbaf06b529da5863db6ad4715bc6c
gaspard <gaspard.petri@etu.utc.fr>
2023-11-02 17:54:38 +0100

    correction exception

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ void test_unitaires(){
         Privilege* instance1 = Privilege::GetInstance();
         Privilege* instance2 = Privilege::GetInstance();
         Privilege* instance3 = Privilege::GetInstance();
-        //Privilege* instance4 = Privilege::GetInstance(); //Lève une exception car la limite est atteinte
+        Privilege* instance4 = Privilege::GetInstance(); //Lève une exception car la limite est atteinte
         std::cout<<"id de la premiere instance : "<<instance1->get_id()<<"\n";
         std::cout<<"id de la seconde instance : "<<instance2->get_id()<<"\n";
         std::cout<<"id de la troisieme instance : "<<instance3->get_id()<<"\n";
-    } catch (const std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+    } catch (const SplendorException& e) {
+        std::cerr << "Exception: " <<e.getInfos()<< std::endl;
     }
 }
