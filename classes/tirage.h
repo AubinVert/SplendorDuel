c9dc2b3fe8d0a778c2851fa3cfac6183b0146041
Quentin Fouinat--Beal <quentin.fouinat-beal@etu.utc.fr>
2023-11-08 17:25:40 +0000

    Update file tirage.h

--- classes/tirage.h
+++ classes/tirage.h
@@ -1,1 +1,19 @@
+#ifndef LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
+#define LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
+#include <iostream>
+#include "carte.h"
+using namespace std;
 
+class Tirage{
+    Pioche* pioche; // ajout de la pioche pour la méthode piocher?
+    int niveau;
+    int nb;
+    int max;
+    Carte_joaillerie ** cartes;
+    Pioche * pioche;
+public:
+    void piocher();
+    // design pattern iterator pour get tirage
+};
+
+#endif //LO21_SPLENDOR_DUEL_PIOCHE_TIRAGE_H
