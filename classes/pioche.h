56cc953cc57ffdd80223e42335fe42485b8c177d
gaspard <gaspard.petri@etu.utc.fr>
2023-11-10 15:55:33 +0100

    correction exception

--- classes/pioche.h
+++ classes/pioche.h
@@ -1,3 +1,4 @@
+#ifndef LO21_SPLENDOR_DUEL_PIOCHE_H
 #define LO21_SPLENDOR_DUEL_PIOCHE_H
 #include <iostream>
 #include "carte.h"
@@ -17,7 +18,8 @@ public:
         return pioche.empty();
     }
 
-    static int getNbPioches(){return nb_pioches;}
+    static int getNbPioches() {return nb_pioches;}
+
 
     ~Pioche(){nb_pioches --;}
 
@@ -28,22 +30,5 @@ public:
         nb_pioches++;
     }
 
-    static void remplirPioche(Pioche* p1, Pioche* p2, Pioche* p3, vector<Carte_joaillerie*>& cartes){
-        if (nb_pioches != max_pioches){throw SplendorException("les pioches ne sont pas toutes créées");}
-
-        for (const Carte_joaillerie* carte : cartes) { //pour chaque carte (ptr constant vers une carte) du vecteur cartes
-            switch(carte->getNiveau()){
-                case 1:
-                    (p1->pioche).push_back(carte);
-                    break;
-                case 2:
-                    (p2->pioche).push_back(carte);
-                    break;
-                case 3:
-                    (p3->pioche).push_back(carte);
-                    break;
-            }
-        }
-    }
-
+    static void remplirPioche(Pioche* p1, Pioche* p2, Pioche* p3, vector<const Carte_joaillerie*>& cartes);
 };
--- main.cpp
+++ main.cpp
@@ -19,12 +19,13 @@ int main(){
 
 void test_unitaires(){
     try {
-        //vector<const Carte_joaillerie*> test = initCartesJoaillerie();
+        vector<const Carte_joaillerie*> test = initCartesJoaillerie();
+        vector<const Carte_joaillerie*>& ref = test;
         Pioche* p1 = new Pioche(1);
         Pioche* p2 = new Pioche(2);
         Pioche* p3 = new Pioche(3);
         //Pioche* p4 = new Pioche(3); //lève une exception
-        //Pioche::remplirPioche(p1, p2, p3, test);
+        Pioche::remplirPioche(p1, p2, p3, ref);
     } catch (const SplendorException& e) {
         std::cerr << "Exception: " <<e.getInfos()<< std::endl;
     }
