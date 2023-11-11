f07177dd7cc88c80d6460e5df2cc32b334958e2c
qfouinat <quentin.fouinat-beal@etu.utc.fr>
2023-11-11 13:37:41 +0100

    premiere ebauche

--- classes/tirage.cpp
+++ classes/tirage.cpp
@@ -1,1 +1,18 @@
+#include "tirage.h"
 
+int Tirage::nb_tirages = 0;
+
+void Tirage::remplirTirage() {
+    while(nb_cartes < max_cartes) {
+        if(!pioche->est_vide()) {
+            pioche[0].push_pack()
+        }
+    }
+}
+
+void testTirage() {
+    Pioche *p1 = new Pioche(1);
+    Tirage *t1 = new Tirage(1, 5, p1);
+    t1->remplirTirage();
+    std::cout<<*t1<<endl;
+}
