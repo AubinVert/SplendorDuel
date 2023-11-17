23dcc17195fcddcb7990c3b1d6c28a49bdfcc240
leo <leopold@lchappuis.fr>
2023-11-17 10:11:22 +0100

    fix nlhomann in CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -18,15 +18,20 @@ int main(){
 
 
 void test_unitaires(){
-    try {
-        Privilege* instance1 = Privilege::GetInstance();
-        Privilege* instance2 = Privilege::GetInstance();
-        Privilege* instance3 = Privilege::GetInstance();
-        Privilege* instance4 = Privilege::GetInstance(); //Lève une exception car la limite est atteinte
-        std::cout<<"id de la premiere instance : "<<instance1->get_id()<<"\n";
-        std::cout<<"id de la seconde instance : "<<instance2->get_id()<<"\n";
-        std::cout<<"id de la troisieme instance : "<<instance3->get_id()<<"\n";
-    } catch (const SplendorException& e) {
-        std::cerr << "Exception: " <<e.getInfos()<< std::endl;
-    }
+
+    testInitCartes();
+
+//    try {
+//        Privilege* instance1 = Privilege::GetInstance();
+//        Privilege* instance2 = Privilege::GetInstance();
+//        Privilege* instance3 = Privilege::GetInstance();
+//        Privilege* instance4 = Privilege::GetInstance(); //Lève une exception car la limite est atteinte
+//        std::cout<<"id de la premiere instance : "<<instance1->get_id()<<"\n";
+//        std::cout<<"id de la seconde instance : "<<instance2->get_id()<<"\n";
+//        std::cout<<"id de la troisieme instance : "<<instance3->get_id()<<"\n";
+//    } catch (const SplendorException& e) {
+//        std::cerr << "Exception: " <<e.getInfos()<< std::endl;
+//    }
+
+
 }
